07109e805e64cce8c9997b1d87078da6d34e4d8f
GeethSameera <geethsameera077@gmail.com>
2019-05-19 21:30:05 +0530

    UI Changes

--- app/src/main/java/com/example/learnafruit/MainActivity.java
+++ app/src/main/java/com/example/learnafruit/MainActivity.java
@@ -16,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_app_icon);
+
     }
 
     public void loadLearn(View view) {
